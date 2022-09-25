8e31ca001473de9e29b9dfd41fbd6262fabe6bdb
ismailinayat <ismailinayat@gmail.com>
2022-09-25 15:37:47 +0500

    fixed mobile responsiveness scale

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
 import '../styles/globals.scss'
 import Navbar from '../components/Navbar'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+
+<Head>
+        <title>Quran With Ayesha</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1"
+        ></meta>
+        <meta name="description" content="We help learning and understanding quran." />
+
+      </Head>
       <Navbar></Navbar>
       <Component {...pageProps} />
     </div>
